412dbc8c82ac1cdb842e5a29e71a5be3523b83fd
Степанян Роберт <sterob@rarus.ru>
2023-04-30 20:58:13 +0300

    feat(404): Добавлена страница 404

---
+++ wp-content/themes/boutique-ps/404.php
@@ -0,0 +1,20 @@
+<?php
+
+get_header()
+
+/* Template Name: main */
+
+
+?>
+
+<div class="container page">
+    <div class="error-404__content">
+        <h1 class="error-404__title">404</h1>
+        <span class="error-404__text">Ой, кажется такой страницы не существует!</span>
+        <a href="/" class="button button--md button--outline-red error-404__button" title="Главная страница">Вернуться
+            на главную</a>
+    </div>
+</div>
+
+<?php
+get_footer() ?>
