68907e28ea773b9a87ef9cf3ccf6a30442171cf1
Степанян Роберт <sterob@rarus.ru>
2023-04-30 21:03:28 +0300

    feat(seo): Добавлено описание сайта и подключена Яндекс Метрика

--- wp-content/themes/boutique-ps/header.php
+++ wp-content/themes/boutique-ps/header.php
@@ -5,13 +5,44 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="yandex-verification" content="498682a5c50c1fdc"/>
+    <meta name="keywords"
+          content="отель, отель в ялте, ялта, бутик, гостиница, номер в ялте, гостиница, жилье в ялте, бутик отель, гостиница в ялте"/>
+    <meta name="description"
+          content="Бутик-отель «Пряности и Страсти» расположен на южном берегу Крыма в самом сердце набережной курортного города Ялта. Гостям предлагается отдых в современных и комфортабельных номерах со всеми удобствами. Номерной фонд бутик-отеля включает в себя 15 совершенно не похожих друг на друга номера."/>
     <link rel="icon" href="/wp-content/themes/boutique-ps/favicon.png" type="image/x-icon">
     <title><?php
         the_title() ?></title>
 
     <?php
     wp_head(); ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+        ym(93400376, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/93400376" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
     <script type='text/javascript'>
         (function (w) {
             var q = [
