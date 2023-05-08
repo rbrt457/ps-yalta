2c9b21367905031b52e97e65d205a1e93c61dd65
Степанян Роберт <sterob@rarus.ru>
2023-05-08 20:57:07 +0300

    feat(project): верстка списочной номеров

--- wp-content/themes/boutique-ps/page-rooms.php
+++ wp-content/themes/boutique-ps/page-rooms.php
@@ -34,8 +34,10 @@ get_header()
                                 <?php
                                 foreach ($roomImages as $roomImage) { ?>
                                     <div class="room__slide swiper-slide">
-                                        <a class="room__image-link foobox" href="<?php echo $roomImage['url'] ?>">
-                                            <img class="room__image" src="<?php echo $roomImage['url'] ?>" alt="<?php
+                                        <a class="room__image-link foobox" href="<?php
+                                        echo $roomImage['url'] ?>">
+                                            <img class="room__image" src="<?php
+                                            echo $roomImage['url'] ?>" alt="<?php
                                             echo $roomImage['alt'] ?>">
                                         </a>
                                     </div>
@@ -45,8 +47,12 @@ get_header()
                         </div>
                     </div>
                     <div class="room__info">
-                        <h3 class="room__title h3"><?php
-                            the_field('roomTitle', $roomId); ?></h3>
+                        <div>
+                            <h2 class="room__title h2 h-sm-28"><?php
+                                the_field('roomTitle', $roomId); ?></h2>
+                            <span class="room__category p5"><?php
+                                the_field('roomCategory', $roomId); ?></span>
+                        </div>
                         <div class="room__description">
                             <?php
                             the_field('roomDescription', $roomId);
@@ -54,7 +60,9 @@ get_header()
                             ?>
 
                         </div>
-                        <a href="<?php the_permalink($roomId) ?>">Подробнее</a>
+                        <a href="<?php
+                        the_permalink($roomId) ?>" class="p2 room__about button button--outline-red button--md"
+                           title="Подробнее о номере">О номере</a>
                     </div>
                 </div>
             </div>
@@ -62,7 +70,8 @@ get_header()
         }
         wp_reset_postdata(); ?>
     </div>
-    <?php get_template_part('components/yandex-map/yandex-map') ?>
+    <?php
+    get_template_part('components/yandex-map/yandex-map') ?>
 </div>
 
 <?php
