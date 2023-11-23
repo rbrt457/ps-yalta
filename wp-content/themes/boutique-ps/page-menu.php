2a98118179ad36345ccff28477877e5316bc63f7
Роберт Степанян <sterob@rarus.ru>
2023-11-24 00:40:29 +0300

    feat(PS-YALTA-24.11.23): добавлена информация о НГ в ресторане апельсин

--- wp-content/themes/boutique-ps/page-menu.php
+++ wp-content/themes/boutique-ps/page-menu.php
@@ -6,142 +6,182 @@ get_header()
 
 ?>
 
-<div class="container page">
-    <h1 class="page__title"><?php
-        the_title() ?></h1>
+<div class="container page content">
+	<h1 class="page__title"><?php
+          the_title() ?></h1>
 
-    <section class="section section--mb-40 content">
-        <h2 class="h2 h-sm-28 section__title">Апельсин "Пряности и Страсти"</h2>
+	<section class="section section--mb-40">
+		<h2 class="h2 h-sm-28 section__title">Апельсин "Пряности и Страсти"</h2>
 
-        <p>На территории отеля расположен ресторан
-            <a href="https://apelsincafe.com/restaurant/pryanosti-i-strasti/"
-               class="link link--bold link--red" target="_blank"
-               title="Апельсин 'Пряности и Страсти'">
-                Апельсин "Пряности и Страсти"</a>.
-            Меню сочетает в себе локальную Крымскую кухню, Кавказскую и Европейскую.
-            Работает ежедневно с 10:00 утра до 23:00.</p>
-        <p><b>Завтраки:</b> Для гостей отеля предоставляется скидка в размере 25% на всё меню с 10:00 до 13:00, в
-            ресторане "Пряности и Страсти" Апельсин, на территории отеля. Скидка предоставляется при предъявлении
-            ключей.</p>
-        <p><b>Рум-сервис:</b> Ресторан Апельсин "Пряности и Страсти" предоставляет услугу рум-сервиса, стоимость данной
-            услуги составляет 10% от счёта.</p>
-        <p><b>Номер для заказа:</b> <a href="tel:79789218792" class="link link--bold link--red">+7(978) 921-87-92</a>
-        </p>
+		<p>На территории отеля расположен ресторан
+			<a href="https://apelsincafe.com/restaurant/pryanosti-i-strasti/"
+			   class="link link--bold link--red" target="_blank"
+			   title="Апельсин 'Пряности и Страсти'">
+				Апельсин "Пряности и Страсти"</a>.
+			Меню сочетает в себе локальную Крымскую кухню, Кавказскую и Европейскую.
+			Работает ежедневно с 10:00 утра до 23:00.</p>
+		<p><b>Завтраки:</b> Для гостей отеля предоставляется скидка в размере 25% на всё меню с 10:00 до 13:00, в
+			ресторане "Пряности и Страсти" Апельсин, на территории отеля. Скидка предоставляется при предъявлении
+			ключей.</p>
+		<p><b>Рум-сервис:</b> Ресторан Апельсин "Пряности и Страсти" предоставляет услугу рум-сервиса, стоимость
+			данной
+			услуги составляет 10% от счёта.</p>
+		<p><b>Номер для заказа:</b> <a href="tel:79789218792" class="link link--bold link--red">+7(978)
+				921-87-92</a>
+		</p>
 
-    </section>
+	</section>
 
-    <section class="section">
-        <h3 class="h3 h-sm-18 section__title">Меню ресторана</h3>
+	<section class="section">
+		<h3 class="h3 h-sm-18 section__title">Меню ресторана</h3>
 
-        <div class="auto-gallery swiper gallery__swiper">
-            <div class="swiper-wrapper foobox">
-                <div class="swiper-slide">
-                    <a href="<?php
-                    echo get_template_directory_uri() ?>/assets/images/menu/menu1.jpg">
-                        <img src="<?php
-                        echo get_template_directory_uri() ?>/assets/images/menu/menu1.jpg"
-                             class="menu-list__image" alt='Меню ресторана "Пряности и Страсти" Апельсин'>
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="<?php
-                    echo get_template_directory_uri() ?>/assets/images/menu/menu2.jpg">
-                        <img src="<?php
-                        echo get_template_directory_uri() ?>/assets/images/menu/menu2.jpg"
-                             class="menu-list__image"
-                             alt='Меню ресторана "Пряности и Страсти" Апельсин'>
-                    </a>
-                </div>
+		<div class="auto-gallery swiper gallery__swiper gallery__swiper--height-auto">
+			<div class="swiper-wrapper foobox">
+				<div class="swiper-slide">
+					<a href="<?php
+                              echo get_template_directory_uri() ?>/assets/images/menu/menu1.jpg">
+						<img src="<?php
+                                    echo get_template_directory_uri() ?>/assets/images/menu/menu1.jpg"
+						     class="menu-list__image" alt='Меню ресторана "Пряности и Страсти" Апельсин'>
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="<?php
+                              echo get_template_directory_uri() ?>/assets/images/menu/menu2.jpg">
+						<img src="<?php
+                                    echo get_template_directory_uri() ?>/assets/images/menu/menu2.jpg"
+						     class="menu-list__image"
+						     alt='Меню ресторана "Пряности и Страсти" Апельсин'>
+					</a>
+				</div>
 
-            </div>
-        </div>
+			</div>
+		</div>
 
-    </section>
-    <section class="section">
-        <h2 class="h2 h-sm-28 section__title">Grande Terrasse Cafe</h2>
-        <p>А также гостям отеля предоставляется скидка в размере 20% в ресторане <a href="https://gt-cafe.ru/"
-                                                                                    target="_blank"
-                                                                                    class="link link--red link--bold">Grande
-                Terrasse
-                Cafe</a> ежедневно с 10:00 до 17:00. Скидка предоставляется при предъявлении ключей.</p>
-        <br>
-        <p>Адрес ресторана: <a class="link link--red" target="_blank"
-                               href="https://yandex.ru/maps/org/gt_cafe/236632501598/?ll=34.160874%2C44.487180&source=morda&z=12">Ялта,
-                ул. набережная им. В.И. Ленина, 18-20</a></p>
-        <br>
-        <h3 class="h3 h-sm-18 section__title">Фото</h3>
-        <div class="auto-gallery swiper gallery__swiper">
-            <div class="swiper-wrapper foobox">
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL.webp" class="foobox">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL.webp" alt="Ресторан GT Cafe"
-                             class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(1).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(1).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(2).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(2).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(3).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(3).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(4).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(4).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(5).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(5).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(6).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(6).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(7).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(7).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(8).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(8).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(9).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(9).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
-                <div class="swiper-slide">
-                    <a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(10).webp">
-                        <img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(10).webp"
-                             alt="Ресторан GT Cafe" class="gallery__image">
-                    </a>
-                </div>
+	</section>
 
-            </div>
-        </div>
-    </section>
+	<section class="section">
+		<h3 class="h3 h-sm-18 section__title">Новогоднее предложение</h3>
+
+		<div>
+			<p>Новый год 🎄 2024🥂</p>
+			<p>Apelsin project «Пряности и страсти»</p>
+			<p>Стоимость Новогодней ночи:</p>
+			<ul>
+				<li>Детский билет 8000₽, включая новогодний подарок</li>
+				<li>Взрослый билет 12000₽</li>
+			</ul>
+			<p>Полная оплата за новогоднюю ночь обязательна, а так же предзаказ по горячему блюду и крепкому
+				алкоголю.</p>
+			<p>Все подробности и бронирование по телефону <a href="tel:79789218792"
+											 class="link link--bold link--red">+7(978)
+					921-87-92</a></p>
+		</div>
+
+		<div class="auto-gallery swiper gallery__swiper gallery__swiper--height-auto">
+			<div class="swiper-wrapper foobox">
+				<div class="swiper-slide">
+					<a href="<?php echo get_template_directory_uri() ?>/assets/images/menu/menu-ny-1.jpg">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/images/menu/menu-ny-1.jpg"
+						     class="menu-list__image" alt='Апельсин. Новогоднее предложение.'>
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="<?php echo get_template_directory_uri() ?>/assets/images/menu/menu-ny-2.jpg">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/images/menu/menu-ny-2.jpg"
+						     class="menu-list__image" alt='Апельсин. Новогоднее меню.'>
+					</a>
+				</div>
+
+			</div>
+		</div>
+
+	</section>
+
+	<section class="section">
+		<h2 class="h2 h-sm-28 section__title">Grande Terrasse Cafe</h2>
+		<p>А также гостям отеля предоставляется скидка в размере 20% в ресторане <a href="https://gt-cafe.ru/"
+														    target="_blank"
+														    class="link link--red link--bold">Grande
+				Terrasse
+				Cafe</a> ежедневно с 10:00 до 17:00. Скидка предоставляется при предъявлении ключей.</p>
+		<p>Адрес ресторана: <a class="link link--red" target="_blank"
+					     href="https://yandex.ru/maps/org/gt_cafe/236632501598/?ll=34.160874%2C44.487180&source=morda&z=12">Ялта,
+				ул. набережная им. В.И. Ленина, 18-20</a></p>
+		<h3 class="h3 h-sm-18 section__title">Фото</h3>
+		<div class="auto-gallery swiper gallery__swiper">
+			<div class="swiper-wrapper foobox">
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL.webp" class="foobox">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL.webp"
+						     alt="Ресторан GT Cafe"
+						     class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(1).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(1).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(2).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(2).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(3).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(3).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(4).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(4).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(5).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(5).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(6).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(6).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(7).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(7).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(8).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(8).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(9).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(9).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+				<div class="swiper-slide">
+					<a href="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(10).webp">
+						<img src="/wp-content/themes/boutique-ps/assets/images/gt-cafe/XXXL(10).webp"
+						     alt="Ресторан GT Cafe" class="gallery__image">
+					</a>
+				</div>
+
+			</div>
+		</div>
+	</section>
 </div>
 
 <?php
